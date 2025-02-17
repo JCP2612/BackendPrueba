cab8d53b8ad71d08fde47e140ec5d5a39987de70
JCP2612 <jurycediel@gmail.com>
2025-02-17 15:37:23 -0500

    Prueba

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,11 @@ import { AuthModule } from './Modules/auth/auth.module';
 import { UsersModule } from './Modules/users/users.module';
 import { TareasModule } from './Modules/tasks/tareas.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
+
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/dbtasks'),
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
     TareasModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/dbtasks'),
     AuthModule,
     UsersModule,
   ],
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
-
-  const configService: ConfigService = app.get<ConfigService>(ConfigService);
-  const port: number =
-    configService.get<number>('PORT') ??
-    parseInt(process.env.PORT || '3000', 10); // Maneja el puerto dinámico de Railway
-
-  console.log(`Server is running on port ${port}`);
-  await app.listen(port);
+  app.setGlobalPrefix('api');
+  app.enableCors();
+  await app.listen(process.env.PORT ?? 3000);
 }
-
 bootstrap();
