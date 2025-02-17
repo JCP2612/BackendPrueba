e7f7ec9ddfe31b2ee62675613ba3b67304d00209
JCP2612 <jurycediel@gmail.com>
2025-02-17 14:41:12 -0500

    nest env deploy

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  app.setGlobalPrefix('api');
   app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
+
+  const configService: ConfigService = app.get<ConfigService>(ConfigService);
+  const port: number =
+    configService.get<number>('PORT') ??
+    parseInt(process.env.PORT || '3000', 10); // Maneja el puerto dinámico de Railway
+
+  console.log(`Server is running on port ${port}`);
+  await app.listen(port);
 }
+
 bootstrap();
